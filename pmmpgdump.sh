30da3eded475c790c206382e14329a8c4958acc8
Daniel Guzmán Burgos <7537643+nethalo@users.noreply.github.com>
2023-07-25 09:48:19 -0500

    dump every 1 min

--- pmmpgdump.sh
+++ pmmpgdump.sh
@@ -5,5 +5,5 @@ while true; do
   echo "Running pg_dump -Upmm-managed --dbname=pmm-managed --inserts --data-only --disable-triggers > /srv/clickhouse/data/pmm/pgpmm/data.RawBLOB"
   pg_dump -Upmm-managed --dbname=pmm-managed --inserts --data-only --disable-triggers > /srv/clickhouse/data/pmm/pgpmm/data.RawBLOB
   echo $?
-  sleep 30m
+  sleep 1m
 done
