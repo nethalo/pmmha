d02e3f680a5034141911144ba1a04a33ac2ea6c4
Daniel Guzmán Burgos <7537643+nethalo@users.noreply.github.com>
2023-11-17 11:16:03 -0500

    support for non-sqlite

--- pmm.sh
+++ pmm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# daniel.burgos@percona.com 
-# 
+# daniel.burgos@percona.com
+#
 
 clear
 set -o pipefail
@@ -62,56 +62,64 @@ function setLockFile () {
 
 
 function installGum () {
-	which dpkg &> /dev/null
-	EXITSTATUS=$?
-	if [ $EXITSTATUS -eq 0 ]; then
-		curl -OL https://github.com/charmbracelet/gum/releases/download/v0.10.0/gum_0.10.0_amd64.deb
-		dpkg -i gum_0.10.0_amd64.deb
-		logInfo "[OK] Installed 'gum' bin"
-		return 0
-	fi
+        which dpkg &> /dev/null
+        EXITSTATUS=$?
+        if [ $EXITSTATUS -eq 0 ]; then
+                curl -OL https://github.com/charmbracelet/gum/releases/download/v0.10.0/gum_0.10.0_amd64.deb
+                dpkg -i gum_0.10.0_amd64.deb
+                logInfo "[OK] Installed 'gum' bin"
+                return 0
+        fi
 
-	which rpm &> /dev/null
-	EXITSTATUS=$?
-	if [ $EXITSTATUS -eq 0 ]; then
-		curl -OL https://github.com/charmbracelet/gum/releases/download/v0.10.0/gum-0.10.0.x86_64.rpm
-		sudo rpm -i gum-0.10.0.x86_64.rpm
-		return 0
-	fi
+        which rpm &> /dev/null
+        EXITSTATUS=$?
+        if [ $EXITSTATUS -eq 0 ]; then
+                curl -OL https://github.com/charmbracelet/gum/releases/download/v0.10.0/gum-0.10.0.x86_64.rpm
+                sudo rpm -i gum-0.10.0.x86_64.rpm
+                return 0
+        fi
 }
 
 
 function verifyGum () {
-	which gum &> /dev/null
-	EXITSTATUS=$?
+        which gum &> /dev/null
+        EXITSTATUS=$?
         #verifyExecution "$EXITSTATUS" "Cannot find gum tool" false
 
     if [ $EXITSTATUS -eq 0 ]; then
-		logInfo "[OK] Found 'gum' bin"
-		return 0
-	fi
+                logInfo "[OK] Found 'gum' bin"
+                return 0
+        fi
 
-	installGum 
+        installGum
 }
 
 
 function installPMM () {
-	curl -L https://www.percona.com/get/pmm | /bin/bash
+        curl -L https://www.percona.com/get/pmm | /bin/bash
 }
 
 function pmmheader () {
 
-	echo '{{ Bold "Percona" }} {{ Italic "Labs" }} {{ Color "99" "0" " PMM CLI " }}' | gum format --theme=light -t template	
+        echo '{{ Bold "Percona" }} {{ Italic "Labs" }} {{ Color "99" "0" " PMM CLI " }}' | gum format --theme=light -t template
 
-	I=$(gum style --padding "1 5" --border rounded --border-foreground 212 "PERCONA")
-	LOVE=$(gum style --padding "1 4" --border thick --border-foreground 57 "LABS")
-	BUBBLE=$(gum style --padding "1 8" --border normal --border-foreground 255 "PMM")
-	GUM=$(gum style --padding "1 5" --border double --border-foreground 240 "CLI")
+        I=$(gum style --padding "1 5" --border rounded --border-foreground 212 "PERCONA")
+        LOVE=$(gum style --padding "1 4" --border thick --border-foreground 57 "LABS")
+        BUBBLE=$(gum style --padding "1 8" --border normal --border-foreground 255 "PMM")
+        GUM=$(gum style --padding "1 5" --border double --border-foreground 240 "CLI")
 
-	I_LOVE=$(gum join "$I" "$LOVE")
-	BUBBLE_GUM=$(gum join "$BUBBLE" "$GUM")
-	gum join --align center --vertical "$I_LOVE" "$BUBBLE_GUM"
-	
+        I_LOVE=$(gum join "$I" "$LOVE")
+        BUBBLE_GUM=$(gum join "$BUBBLE" "$GUM")
+        gum join --align center --vertical "$I_LOVE" "$BUBBLE_GUM"
+
+}
+
+function getVersion () {
+        USER=$1
+        PASS=$2
+        URL=$3
+        VERVAL=$(curl -s -k -X 'GET'   "https://${USER}:${PASS}@${URL}/v1/version"   -H 'accept: application/json' | jq '.version' | awk -F. '{print $2}')
+
 }
 
 setLockFile
@@ -123,114 +131,125 @@ gum confirm "Confirms: $ACTION"
 CONFIRMED=$?
 
 if [ $CONFIRMED -eq 0 ]; then
-	if [[ $ACTION == "Install PMM" ]]; then
-		installPMM
-	fi
+        if [[ $ACTION == "Install PMM" ]]; then
+                installPMM
+        fi
 
-	if [[ $ACTION == "Set a PMM replica" ]]; then
-		clear
-		echo "# PMM Replica" | gum format 
-		echo "# Using VictoriaMetrics Federation" | gum format 
-		echo ":candy:" | gum format -t emoji
-		PRIMARY=$(gum input --header="Host / IP of the Primary PMM server" --placeholder "192.168.1.10" --prompt.foreground="99" --cursor.foreground="99" --header.foreground="240")
-		PORT="443"
-		USER=$(gum input --header="Primary PMM USER" --value "admin" --placeholder "admin")
-		PASS=$(gum input --password  --header="Primary PMM PASSWORD")
-		SCHEME="https"
+        if [[ $ACTION == "Set a PMM replica" ]]; then
+                clear
+                echo "# PMM Replica" | gum format
+                echo "# Using VictoriaMetrics Federation" | gum format
+                echo ":candy:" | gum format -t emoji
+                PRIMARY=$(gum input --header="Host / IP of the Primary PMM server" --placeholder "192.168.1.10" --prompt.foreground="99" --cursor.foreground="99" --header.foreground="240")
+                PORT="443"
+                USER=$(gum input --header="Primary PMM USER" --value "admin" --placeholder "admin")
+                PASS=$(gum input --password  --header="Primary PMM PASSWORD")
+                SCHEME="https"
 
         clear
-		echo '{{ Bold "A new scrap will be added" }}' | gum format -t template
-		# cat $scrapeyaml | gum format -t code -l yaml
-		export jobname=$JOBNAME
-		export scheme=$SCHEME
-		export user=$USER
-		export pass=$PASS
-		export host=$PRIMARY
-		export port=$PORT
-		envsubst < $scrapeyaml > /tmp/pepe
-
-		
-		echo '{{ Bold "Creating: File with pmmserver IP/Host" }}' | gum format -t template
-		echo $PRIMARY > /tmp/pmmserver.txt
-		sudo docker cp -q /tmp/pmmserver.txt pmm-server:/srv/pmmserver.txt
-
-		echo '{{ Bold "Creating: Replica table on ClickHouse" }}' | gum format -t template
-		sudo docker cp -q metrics.sql pmm-server:/tmp/metrics.sql
-		sudo docker exec pmm-server chown pmm:pmm /tmp/metrics.sql
-		sudo docker exec pmm-server bash -c "clickhouse-client < /tmp/metrics.sql"
+                getVersion $USER $PASS $PRIMARY
+                echo '{{ Bold "A new scrap will be added" }}' | gum format -t template
+                # cat $scrapeyaml | gum format -t code -l yaml
+                export jobname=$JOBNAME
+                export scheme=$SCHEME
+                export user=$USER
+                export pass=$PASS
+                export host=$PRIMARY
+                export port=$PORT
+                envsubst < $scrapeyaml > /tmp/pepe
 
 
-		echo '{{ Bold "Creating: Scraper YAML file" }}' | gum format -t template
-		sudo docker cp -q /tmp/pepe pmm-server:/srv/prometheus/prometheus.base.yml
-		sudo docker exec pmm-server chown pmm:pmm /srv/prometheus/prometheus.base.yml
-		
-		echo '{{ Bold "Creating: QAN and Inventory replica scripts" }}' | gum format -t template
-		sudo docker cp -q qanreplica.sh pmm-server:/srv/qanreplica.sh
-		sudo docker exec pmm-server chmod +x /srv/qanreplica.sh
-		sudo docker cp -q inventoryreplica.sh pmm-server:/srv/inventoryreplica.sh
-		sudo docker exec pmm-server chmod +x /srv/inventoryreplica.sh
+                echo '{{ Bold "Creating: File with pmmserver IP/Host" }}' | gum format -t template
+                echo $PRIMARY > /tmp/pmmserver.txt
+                sudo docker cp -q /tmp/pmmserver.txt pmm-server:/srv/pmmserver.txt
+
+                echo '{{ Bold "Creating: Replica table on ClickHouse" }}' | gum format -t template
+                sudo docker cp -q metrics.sql pmm-server:/tmp/metrics.sql
+                sudo docker exec pmm-server chown pmm:pmm /tmp/metrics.sql
+                sudo docker exec pmm-server bash -c "clickhouse-client < /tmp/metrics.sql"
 
 
-		echo '{{ Bold "Creating: QAN and Inventory supervisord ini files" }}' | gum format -t template
-		sudo docker cp -q qanreplica.ini pmm-server:/etc/supervisord.d/qanreplica.ini
-		sudo docker cp -q inventoryreplica.ini pmm-server:/etc/supervisord.d/inventoryreplica.ini
+                echo '{{ Bold "Creating: Scraper YAML file" }}' | gum format -t template
+                sudo docker cp -q /tmp/pepe pmm-server:/srv/prometheus/prometheus.base.yml
+                sudo docker exec pmm-server chown pmm:pmm /srv/prometheus/prometheus.base.yml
 
-		echo '{{ Bold "Creating: SQLite replica scripts" }}' | gum format -t template
-		sudo docker cp -q restoresqlitepmm.sh pmm-server:/srv/restoresqlitepmm.sh
-		sudo docker exec pmm-server chmod +x /srv/restoresqlitepmm.sh
-		sudo docker cp -q restoresqlitedash.sh pmm-server:/srv/restoresqlitedash.sh
-		sudo docker exec pmm-server chmod +x /srv/restoresqlitedash.sh
+                echo '{{ Bold "Creating: QAN and Inventory replica scripts" }}' | gum format -t template
+                sudo docker cp -q qanreplica.sh pmm-server:/srv/qanreplica.sh
+                sudo docker exec pmm-server chmod +x /srv/qanreplica.sh
+                sudo docker cp -q inventoryreplica.sh pmm-server:/srv/inventoryreplica.sh
+                sudo docker exec pmm-server chmod +x /srv/inventoryreplica.sh
 
-		echo '{{ Bold "Creating: SQLite supervisord ini files" }}' | gum format -t template
-		sudo docker cp -q restoresqlitepmm.ini pmm-server:/etc/supervisord.d/restoresqlitepmm.ini
-		sudo docker cp -q restoresqlitedash.ini pmm-server:/etc/supervisord.d/restoresqlitedash.ini
 
-		gum spin --show-output --spinner monkey --title "Loading..." --title.foreground 99 -- sh -c 'sudo docker exec -d pmm-server supervisorctl restart pmm-agent &> /dev/null; sleep 5; echo "PMM Metrics Replica set"'
-		gum spin --show-output --spinner monkey --title "Loading..." --title.foreground 99 -- sh -c 'sudo docker exec -d pmm-server supervisorctl update &> /dev/null; sleep 5; echo "PMM QAN + Inventory Replica set"'
+                echo '{{ Bold "Creating: QAN and Inventory supervisord ini files" }}' | gum format -t template
+                sudo docker cp -q qanreplica.ini pmm-server:/etc/supervisord.d/qanreplica.ini
+                sudo docker cp -q inventoryreplica.ini pmm-server:/etc/supervisord.d/inventoryreplica.ini
 
-	fi
+                if [ $VERVAL -lt 40  ]; then
+                        echo '{{ Bold "Creating: SQLite replica scripts" }}' | gum format -t template
+                        sudo docker cp -q restoresqlitepmm.sh pmm-server:/srv/restoresqlitepmm.sh
+                        sudo docker exec pmm-server chmod +x /srv/restoresqlitepmm.sh
+                        sudo docker cp -q restoresqlitedash.sh pmm-server:/srv/restoresqlitedash.sh
+                        sudo docker exec pmm-server chmod +x /srv/restoresqlitedash.sh
 
-	if [[ $ACTION == "Set a PMM Primary" ]]; then
-		
-		echo '{{ Bold "Creating: Inventory table on ClickHouse" }}' | gum format -t template
+                        echo '{{ Bold "Creating: SQLite supervisord ini files" }}' | gum format -t template
+                        sudo docker cp -q restoresqlitepmm.ini pmm-server:/etc/supervisord.d/restoresqlitepmm.ini
+                        sudo docker cp -q restoresqlitedash.ini pmm-server:/etc/supervisord.d/restoresqlitedash.ini
+
+                fi
+
+                gum spin --show-output --spinner monkey --title "Loading..." --title.foreground 99 -- sh -c 'sudo docker exec -d pmm-server supervisorctl restart pmm-agent &> /dev/null; sleep 5; echo "PMM Metrics Replica set"'
+                gum spin --show-output --spinner monkey --title "Loading..." --title.foreground 99 -- sh -c 'sudo docker exec -d pmm-server supervisorctl update &> /dev/null; sleep 5; echo "PMM QAN + Inventory Replica set"'
+
+        fi
+
+        if [[ $ACTION == "Set a PMM Primary" ]]; then
+
+                VERVAL=$(docker exec pmm-server pmm-admin -v | grep PMMVersion | awk -F: '{print $2}' | awk -F. '{print $2}')
+                echo $VERVAL
+
+                echo '{{ Bold "Creating: Inventory table on ClickHouse" }}' | gum format -t template
                 sudo docker cp -q pgpmm.sql pmm-server:/tmp/pgpmm.sql
-		sudo docker exec pmm-server chown pmm:pmm /tmp/pgpmm.sql
-		sudo docker exec pmm-server bash -c "clickhouse-client < /tmp/pgpmm.sql"
+                sudo docker exec pmm-server chown pmm:pmm /tmp/pgpmm.sql
+                sudo docker exec pmm-server bash -c "clickhouse-client < /tmp/pgpmm.sql"
 
-		echo '{{ Bold "Creating: Inventory primary script" }}' | gum format -t template
-		sudo docker cp -q pmmpgdump.sh pmm-server:/srv/pmmpgdump.sh
-		sudo docker exec pmm-server chmod +x /srv/pmmpgdump.sh
+                echo '{{ Bold "Creating: Inventory primary script" }}' | gum format -t template
+                sudo docker cp -q pmmpgdump.sh pmm-server:/srv/pmmpgdump.sh
+                sudo docker exec pmm-server chmod +x /srv/pmmpgdump.sh
 
-		echo '{{ Bold "Creating: Inventory supervisord ini files" }}' | gum format -t template
-		sudo docker cp -q pmmpgdump.ini pmm-server:/etc/supervisord.d/pmmpgdump.ini
+                echo '{{ Bold "Creating: Inventory supervisord ini files" }}' | gum format -t template
+                sudo docker cp -q pmmpgdump.ini pmm-server:/etc/supervisord.d/pmmpgdump.ini
 
-		echo '{{ Bold "Creating: sqlite tables on ClickHouse" }}' | gum format -t template
-		sudo docker cp -q sqlitepmm.sql pmm-server:/tmp/sqlitepmm.sql
-		sudo docker exec pmm-server chown pmm:pmm /tmp/sqlitepmm.sql
-		sudo docker exec pmm-server bash -c "clickhouse-client < /tmp/sqlitepmm.sql"
+                if [ $VERVAL -lt 40  ]; then
 
-                sudo docker cp -q sqlitedash.sql pmm-server:/tmp/sqlitedash.sql
-		sudo docker exec pmm-server chown pmm:pmm /tmp/sqlitedash.sql
-		sudo docker exec pmm-server bash -c "clickhouse-client < /tmp/sqlitedash.sql"
+                        echo '{{ Bold "Creating: sqlite tables on ClickHouse" }}' | gum format -t template
+                        sudo docker cp -q sqlitepmm.sql pmm-server:/tmp/sqlitepmm.sql
+                        sudo docker exec pmm-server chown pmm:pmm /tmp/sqlitepmm.sql
+                        sudo docker exec pmm-server bash -c "clickhouse-client < /tmp/sqlitepmm.sql"
 
-		echo '{{ Bold "Creating: SQLite primary script" }}' | gum format -t template
-		sudo docker cp -q sqlitepmm.sh pmm-server:/srv/sqlitepmm.sh
-		sudo docker exec pmm-server chmod +x /srv/sqlitepmm.sh
+                        sudo docker cp -q sqlitedash.sql pmm-server:/tmp/sqlitedash.sql
+                        sudo docker exec pmm-server chown pmm:pmm /tmp/sqlitedash.sql
+                        sudo docker exec pmm-server bash -c "clickhouse-client < /tmp/sqlitedash.sql"
 
-		sudo docker cp -q sqlitedash.sh pmm-server:/srv/sqlitedash.sh
-		sudo docker exec pmm-server chmod +x /srv/sqlitedash.sh
+                        echo '{{ Bold "Creating: SQLite primary script" }}' | gum format -t template
+                        sudo docker cp -q sqlitepmm.sh pmm-server:/srv/sqlitepmm.sh
+                        sudo docker exec pmm-server chmod +x /srv/sqlitepmm.sh
 
-		echo '{{ Bold "Creating: sqlite supervisord ini files" }}' | gum format -t template
-		sudo docker cp -q sqlitepmm.ini pmm-server:/etc/supervisord.d/sqlitepmm.ini
-		sudo docker cp -q sqlitedash.ini pmm-server:/etc/supervisord.d/sqlitedash.ini
+                        sudo docker cp -q sqlitedash.sh pmm-server:/srv/sqlitedash.sh
+                        sudo docker exec pmm-server chmod +x /srv/sqlitedash.sh
 
-		echo '{{ Bold "Adding: ClickHouse Port Forwarding" }}' | gum format -t template
-		docker ps -a --format '{{.Names}}' | grep -q socatpmm
-		if [ $? -eq 1 ]; then
+                        echo '{{ Bold "Creating: sqlite supervisord ini files" }}' | gum format -t template
+                        sudo docker cp -q sqlitepmm.ini pmm-server:/etc/supervisord.d/sqlitepmm.ini
+                        sudo docker cp -q sqlitedash.ini pmm-server:/etc/supervisord.d/sqlitedash.ini
+
+                fi
+
+                echo '{{ Bold "Adding: ClickHouse Port Forwarding" }}' | gum format -t template
+                docker ps -a --format '{{.Names}}' | grep -q socatpmm
+                if [ $? -eq 1 ]; then
                         docker run --detach --restart unless-stopped --publish 9000:9000 --link pmm-server:target --name socatpmm alpine/socat tcp-listen:9000,fork,reuseaddr tcp-connect:target:9000
-		fi
+                fi
 
-		gum spin --show-output --spinner monkey --title "Loading..." --title.foreground 99 -- sh -c 'sudo docker exec -d pmm-server supervisorctl update &> /dev/null; sleep 5; echo "PMM Primary Set"'
+                gum spin --show-output --spinner monkey --title "Loading..." --title.foreground 99 -- sh -c 'sudo docker exec -d pmm-server supervisorctl update &> /dev/null; sleep 5; echo "PMM Primary Set"'
 
-	fi
+        fi
 fi
