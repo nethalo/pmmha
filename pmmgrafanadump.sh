cc039f6066e7818c6423488ca600fa7871833d16
Daniel Guzmán Burgos <7537643+nethalo@users.noreply.github.com>
2023-12-08 08:23:58 -0500

    Update pmmgrafanadump.sh set clean to drop tables

--- pmmgrafanadump.sh
+++ pmmgrafanadump.sh
@@ -2,8 +2,8 @@
 
 while true; do
   # Your command here
-  echo "Running pg_dump -Ugrafana --dbname=grafana --inserts --data-only --disable-trigger > /srv/clickhouse/data/pmm/pggrafana/data.RawBLOB"
-  pg_dump -Ugrafana --dbname=grafana --inserts --data-only --disable-trigger > /srv/clickhouse/data/pmm/pggrafana/data.RawBLOB
+  echo "Running pg_dump -Ugrafana --dbname=grafana --inserts --disable-trigger --exclude-table=user_auth_token -c > /srv/clickhouse/data/pmm/pggrafana/data.RawBLOB"
+  pg_dump -Ugrafana --dbname=grafana --inserts --disable-trigger --exclude-table=user_auth_token -c > /srv/clickhouse/data/pmm/pggrafana/data.RawBLOB
   echo $?
   sleep 1m
 done
